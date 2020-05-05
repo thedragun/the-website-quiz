04a5053693d8d202ae480e702d3fd44ecab1fadd
thedragun <62728488+thedragun@users.noreply.github.com>
2020-05-05 14:27:23 +1200

    v0.6

--- script.js
+++ script.js
@@ -22,12 +22,69 @@ const myQuestions = [
       correctAnswer: "a"
     },
 
-
-
 ]
 
 
-function buildQuiz() {}
+function buildQuiz() {
+// html output
+  const output = [];
+
+    myQuestions.forEach(
+        (currentQuestion, questionNumber) => {
+        //answer storage
+        const answers = [];
+
+        //for each answer
+        for(letter in currentQuestion.answers){
+
+        //add a html radio button
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+          );
+        }
+
+        //add this question and its answers to the output
+        output.push(
+           `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+
+    //finally combine the output list into a string of html and put it on the page
+    quizContainer.innerHTML = output.join('');
+}
+
+
+myQuestions.forEach((currentQuestion, questionNumber) => {
+  // we'll want to store the list of answer choices
+const answers = [];
+
+// and for each available answer...
+for(letter in currentQuestion.answers){
+
+  // ...add an html radio button
+  answers.push(
+    `<label>
+      <input type="radio" name="question${questionNumber}" value="${letter}">
+      ${letter} :
+      ${currentQuestion.answers[letter]}
+    </label>`
+  );
+}
+
+// add this question and its answers to the output
+output.push(
+  `<div class="question"> ${currentQuestion.question} </div>
+  <div class="answers"> ${answers.join('')} </div>`
+);
+});
+
+
 
 function showResults() {}
 
